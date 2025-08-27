24a978c3bca1e6ea7235115fa117b7707bc5dc1c
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2025-08-28 03:26:59 +0900

    array object state

--- app/list/page.js
+++ app/list/page.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 export default function List() {
     let products = ['Tomatoes', 'Pasta', 'Coconut'];
-    let [qty, qtyf] = useState(0); // 해당 부분 데이터 변경되면 자동 재렌더링
+    let [qty, qtyf] = useState([0, 0, 0]); // 해당 부분 데이터 변경되면 자동 재렌더링
     return (
         <div>
             <h4 className="title">상품목록</h4>
@@ -14,8 +14,18 @@ export default function List() {
                         <div className="food" key={i}>
                             <img src={`food${i}.png`} className="food-img"></img>
                             <h4>{a} $40</h4>
-                            <span> {qty} </span>
-                            <button onClick={function () { qtyf(qty + 1) }}>+</button>
+                            <button onClick={function () {
+                                let copyQty = [...qty]; // 독립적인 array 복사본 생성
+                                copyQty[i] = copyQty[i] - 1;
+                                qtyf(copyQty);
+                                // array나 object자료형 state 수정할때는 이렇게 사용해야함
+                            }}>-</button>
+                            <span> {qty[i]} </span>
+                            <button onClick={function () {
+                                let copyQty = [...qty]; // 독립적인 array 복사본 생성
+                                copyQty[i] = copyQty[i] + 1;
+                                qtyf(copyQty);
+                            }}>+</button>
                         </div>
                     )
                 })
