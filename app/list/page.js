c04d12493e30af9c0108bf5650c00c220ade231d
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2025-08-28 03:01:55 +0900

    useState 'use client'

--- app/list/page.js
+++ app/list/page.js
@@ -1,5 +1,10 @@
+'use client'
+
+import { useState } from "react";
+
 export default function List() {
-    let products = ['Tomatoes', 'Pasta', 'Coconut']
+    let products = ['Tomatoes', 'Pasta', 'Coconut'];
+    let [qty, qtyf] = useState(0); // 해당 부분 데이터 변경되면 자동 재렌더링
     return (
         <div>
             <h4 className="title">상품목록</h4>
@@ -9,8 +14,8 @@ export default function List() {
                         <div className="food" key={i}>
                             <img src={`food${i}.png`} className="food-img"></img>
                             <h4>{a} $40</h4>
-                            <span> 1 </span>
-                            <button>+</button>
+                            <span> {qty} </span>
+                            <button onClick={function () { qtyf(qty + 1) }}>+</button>
                         </div>
                     )
                 })
